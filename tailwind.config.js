ebfcde595a561700e6427b886bb3610e1a40e216
HugoSbl <hugo.sablot@gmail.com>
2023-07-21 22:59:12 +0200

    vue-test(fix): tailwind content fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,vue,ts}",
+    "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
