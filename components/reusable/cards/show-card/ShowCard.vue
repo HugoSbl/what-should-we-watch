f675ebc06cb1dbb8af91af592d9becb29f3ea6b7
HugoSbl <hugo.sablot@gmail.com>
2023-07-22 14:46:49 +0200

    search_card(feat): searchCard info display

---
+++ components/reusable/cards/show-card/ShowCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="flex h-36 mb-2">
+    <img
+      v-if="image && image.medium"
+      :src="image.medium"
+      alt="Show image"
+      class="rounded-lg w-24"
+    />
+    <div v-else class="w-24">No image</div>
+
+    <div>
+      <div class="ml-2 font-black">{{ title }}</div>
+      <div class="flex">
+        <div class="ml-2 rounded bg-slate-400 inline-block text-white p-1">
+          <div v-if="rating && rating.average">{{ rating.average }}</div>
+          <div v-else>no rating</div>
+        </div>
+        <div class="ml-2 rounded bg-slate-400 inline-block text-white p-1">
+          <div v-if="averageRuntime">{{ averageRuntime }} min</div>
+          <div v-else>unknown runtime</div>
+        </div>
+      </div>
+
+      <div v-if="premiered" class="flex">
+        <div class="ml-2">
+          <div v-if="premiered">{{ premiered.slice(0, 4) }}</div>
+        </div>
+        <div>-</div>
+        <div class="">
+          <div v-if="ended">{{ ended.slice(0, 4) }}</div>
+          <div v-else>ongoing</div>
+        </div>
+      </div>
+      <div v-else class="ml-2">unknown date</div>
+    </div>
+    <div class="left">
+      <button class="ml-auto rounded bg-yellow-300">Add to watchlist</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    links: {
+      type: String,
+      required: false,
+    },
+    premiered: {
+      type: String,
+      required: false,
+    },
+    ended: {
+      type: String,
+      required: false,
+    },
+    rating: {
+      type: Object,
+      required: false,
+    },
+    averageRuntime: {
+      type: Number,
+      required: false,
+    },
+  },
+};
+</script>
--- components/search/Search.vue
+++ components/search/Search.vue
@@ -2,15 +2,25 @@
 import { ref, watchEffect } from "vue";
 import GlobalCards from "../reusable/cards/global-card.vue";
 import { useFetch } from "nuxt/app";
+import ShowCard from "../reusable/cards/show-card/ShowCard.vue";
 
 const searchInput = ref("");
-let fetchResults = ref(null);
+let fetchResults = ref([]);
 
 watchEffect(async () => {
   if (searchInput.value.length > 3) {
-    fetchResults.value = await useFetch(
+    const { data, error, isFetching } = await useFetch(
       `https://api.tvmaze.com/search/shows?q=${searchInput.value}`
     );
+    if (error.value) {
+      console.log("error", error.value);
+      return;
+    }
+    if (isFetching) {
+      console.log("fetching data...");
+      return;
+    }
+    fetchResults.value = data.value;
     console.log("fetch result", fetchResults.value);
     return;
   }
@@ -22,10 +32,22 @@ watchEffect(async () => {
     <div class="flex flex-col">
       <input
         class="w-full border-2"
-        placeholder="search for a show"
         v-model="searchInput"
+        placeholder="search for a show"
       />
       <div>this is a test</div>
+      <div v-if="fetchResults && fetchResults.length > 0 && !isFetching">
+        <div v-for="(show, index) in fetchResults" :key="index">
+          <ShowCard
+            :title="show.show.name"
+            :image="show.show.image"
+            :premiered="show.show.premiered"
+            :ended="show.show.ended"
+            :rating="show.show.rating"
+            :averageRuntime="show.show.averageRuntime"
+          />
+        </div>
+      </div>
     </div>
   </GlobalCards>
 </template>
