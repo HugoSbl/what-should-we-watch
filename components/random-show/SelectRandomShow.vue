25d29335db230d40259d31560c8c9d0d1f79f71d
HugoSbl <hugo.sablot@gmail.com>
2023-07-23 12:47:33 +0200

    select_random(feat): rating function treatment for randomizer

--- components/random-show/SelectRandomShow.vue
+++ components/random-show/SelectRandomShow.vue
@@ -1,12 +1,41 @@
 <template>
   <SectionCard title="What should we watch tonight ?">
     <div class="w-full flex">
-      <n-button>Select a random show from selected one in my list</n-button>
+      <n-button @click="handleRandomizer">
+        Select a random show from selected ones in my list</n-button
+      >
     </div>
   </SectionCard>
 </template>
 
 <script setup>
+import { inject } from "vue";
 import SectionCard from "../reusable/cards/SectionCard.vue";
 import { NButton } from "naive-ui";
+
+const selectedWatchlistShows = inject("selectedWatchlistShows");
+
+const handleRandomizer = () => {
+  if (selectedWatchlistShows.value.length > 0) {
+    const weightedShows = [];
+
+    selectedWatchlistShows.value.forEach((show) => {
+      // Assuming rating is between 0 and 10, you can adjust accordingly
+      const count = Math.floor(show.rating);
+      console.log("show", show);
+      for (let i = 0; i < count; i++) {
+        // Create an array with shows repeated as per their ratings
+        weightedShows.push(show);
+      }
+    });
+
+    const randomIndex = Math.floor(Math.random() * weightedShows.length);
+
+    const selectedShow = weightedShows[randomIndex];
+
+    console.log("Randomly selected show:", selectedShow);
+  } else {
+    console.log("No shows selected");
+  }
+};
 </script>
