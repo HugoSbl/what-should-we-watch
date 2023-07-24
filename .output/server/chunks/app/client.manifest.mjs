697085a2f716a8a25d81359a856a94e41127c0d6
HugoSbl <hugo.sablot@gmail.com>
2023-07-24 17:55:32 +0200

    additionnal_features(feat): vercel

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,89 @@
+const client_manifest = {
+  "_vue.505b826e.7d1269d7.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "vue.505b826e.7d1269d7.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.fdd90155.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.565250f5.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js",
+      "_vue.505b826e.7d1269d7.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.fdd90155.css": {
+    "file": "error-404.fdd90155.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.f4699d7d.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.64fbf86a.js",
+    "imports": [
+      "_vue.505b826e.7d1269d7.js",
+      "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.f4699d7d.css": {
+    "file": "error-500.f4699d7d.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.823d4249.css",
+    "src": "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "css": [
+      "entry.823d4249.css"
+    ],
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.cf0382f7.js",
+    "isEntry": true,
+    "src": "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js",
+    "_globalCSS": true
+  },
+  "entry.823d4249.css": {
+    "file": "entry.823d4249.css",
+    "resourceType": "style"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.4d771c38.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/chunks/app/styles.mjs
@@ -0,0 +1,9 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "node_modules/.pnpm/nuxt@3.6.5_@types+node@18.16.19_eslint@8.45.0/node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.1212c8d5.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.3d3d54d8.mjs').then(interopDefault),
+  "node_modules/.pnpm/@nuxt+ui-templates@1.2.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.53688708.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
