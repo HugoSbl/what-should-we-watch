948c6a86247f2687e8fb4951fd5abe347b5b2d35
HugoSbl <hugo.sablot@gmail.com>
2023-07-24 16:36:07 +0200

    style(feat): what should we watch section

--- components/reusable/cards/show-card/ShowCard.vue
+++ components/reusable/cards/show-card/ShowCard.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="flex h-36 mb-2 justify-between hover:bg-black/20">
     <div class="flex items-center">
-      <div class="w-24">
+      <div class="flex-shrink-0 w-24">
         <img
           v-if="image"
           :src="image.medium"
-          alt="Show image"
           class="rounded-lg aspect-auto"
+          :alt="title"
         />
         <div v-else class="">No image</div>
       </div>
 
-      <div class="space-y-2">
-        <div class="ml-2 font-black text-sm md:text-base line-clamp-1">
+      <div class="md:ml-4 ml-2 space-y-2">
+        <div class="font-black text-sm md:text-base line-clamp-1">
           {{ title }}
         </div>
-        <div class="flex">
+        <div class="inline-flex md:flex-row flex-col md:space-x-1 space-y-1">
           <div
-            class="ml-2 rounded text-sm bg-slate-400/70 inline-block text-white px-1"
+            class="rounded text-sm bg-slate-400/70 inline-block text-white px-1"
           >
-            <div class="flex items-center" v-if="rating && rating.average">
+            <div v-if="rating && rating.average" class="flex items-center">
               {{ rating.average }}<Star class="ml-1" :size="14" />
             </div>
             <div v-else>no rating</div>
           </div>
           <div
-            class="ml-2 rounded text-sm bg-slate-400/70 inline-block text-white px-1"
+            class="rounded text-sm bg-slate-400/70 inline-block text-white px-1"
           >
             <div v-if="averageRuntime">{{ averageRuntime }} min</div>
             <div v-else>unknown runtime</div>
@@ -33,7 +33,7 @@
         </div>
 
         <div v-if="premiered" class="flex">
-          <div class="ml-2">
+          <div class="">
             <div v-if="premiered">{{ premiered.slice(0, 4) }}</div>
           </div>
           <div>-</div>
@@ -42,7 +42,7 @@
             <div v-else>ongoing</div>
           </div>
         </div>
-        <div v-else class="ml-2">unknown date</div>
+        <div v-else class="">unknown date</div>
         <dialog-show-card />
       </div>
     </div>
