dfe21d4ff5d782ad007869f60fdb88471c72ddb2
HugoSbl <hugo.sablot@gmail.com>
2023-07-25 13:32:36 +0200

    toast(feat): add toast

--- components/reusable/cards/SectionCard.vue
+++ components/reusable/cards/SectionCard.vue
@@ -10,9 +10,7 @@ export default {
 </script>
 
 <template>
-  <div
-    class="p-2 rounded-xl border-gray-800 bg-white/10 border-2 border-white/10 m-4"
-  >
+  <div class="p-2 rounded-xl bg-white/10 border-2 border-white/10 m-4">
     <h3 class="font-bold italic text-xl m-2 text-center">{{ title }}</h3>
     <slot></slot>
   </div>
