8b44dc5b6fe07d9a467ac8c35e1001c957f198d1
HugoSbl <hugo.sablot@gmail.com>
2023-07-25 15:27:15 +0200

    main(fix): css in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
